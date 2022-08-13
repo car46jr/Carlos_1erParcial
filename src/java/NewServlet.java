068ffe8ea0a85b0af59009fd18152b966f5047c0
car46jr <91435074+car46jr@users.noreply.github.com>
2022-08-13 09:33:45 -0600

    Clase

--- src/java/NewServlet.java
+++ src/java/NewServlet.java
@@ -27,23 +27,60 @@ public class NewServlet extends HttpServlet {
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+   protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet NewServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet NewServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try ( PrintWriter respuesta = response.getWriter()) {
+            alumno=new Alumno(
+                request.getParameter("codigo"),
+                request.getParameter("nombre"),
+                request.getParameter("correo"),
+                request.getParameter("direccion")
+            );               
+            
+            if(registroAlumno==null){
+                 registroAlumno=new AlumnoController();
+            }
+           
+            registroAlumno.guardarAlumno(alumno);//almacenarlo en el array
+             alumnosRegistrados= registroAlumno.getAlumnos();
+            
+            respuesta.println("<!DOCTYPE html>");
+            respuesta.println("<html>");
+            respuesta.println("<head>");
+            respuesta.println("<title>Servlet NewServlet</title>");   
+            respuesta.println("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css' integrity='sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N' crossorigin='anonymous'>");
+            respuesta.println("<script src='https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js' integrity='sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct' crossorigin='anonymous'></script>");
+            respuesta.println("</head>");
+            respuesta.println("<body>");           
+            respuesta.println("<div class='container'>");
+             respuesta.println("<br><h1>Gracias por registrarse </h1><br>");  
+            respuesta.println("<form name='nombreForm'>");
+            respuesta.println("<div class='container-lg d-flex'> <a href='index.html' class=\"btn btn-success ml-auto\">Registrar Nuevo</a></div><br>");
+            respuesta.println("<table class=\"table table-hover table-striped\">");   
+            respuesta.println("<thead><tr> <th scope=\"col\">CODIGO CLIENTE</th> <th scope=\"col\">NOMBRE DEL CLIENTE</th>\n" +
+                                "<th scope=\"col\">CORREO DEL CLIENTE</th> <th scope=\"col\">DIRECCION</th>\n" +
+                                "<th scope=\"col\">ACCION</th> </tr></thead>");  
+            respuesta.println("<tbody>");
+            for (int i = 0; i < alumnosRegistrados.length; i++){
+                    if(!alumnosRegistrados[i].getCodigo().isEmpty()){
+                       respuesta.println("<tr><td>" + alumnosRegistrados[i].getCodigo()+ "</td>");
+                       respuesta.println("<td>" + alumnosRegistrados[i].getNombre() + "</td>");
+                       respuesta.println("<td>" + alumnosRegistrados[i].getCorreo()+ "</td>");
+                       respuesta.println("<td>" + alumnosRegistrados[i].getDireccion()+ "</td>");
+                       respuesta.println("<td>"
+                               + "<button type=\"button\" class=\"btn btn-warning\"></i>Editar</button> "
+                               + "<button type=\"button\" class=\"btn btn-danger\">Eliminar</button>"
+                               + "</td></tr>");
+                    }
+                }
+            respuesta.println("</tbody></table>");
+            respuesta.println("</div>");
+            respuesta.println("</form>");
+            respuesta.println("</body>");
+            respuesta.println("</html>");
         }
     }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
